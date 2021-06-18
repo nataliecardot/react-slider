59395cd827155cc90a9b8b5543d9b35ae784af53
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-18 12:44:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# react-slider
+
+Very simple app featuring data with previous and next buttons that navigate between list items. Autoslide option (goes to next item in list after a certain amount of time).
+
+To view, run `npm i && npm start` in downloaded project.
+
+This project was completed as part of a [React tutorial series](https://www.youtube.com/watch?v=a_7Z7C_JCyo&ab_channel=freeCodeCamp.org) by John Smilga.
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, voluptas impedit. Rerum et dicta ratione sed, maiores nesciunt qui cum.',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel tempore consequuntur facere dolor quisquam ullam sunt id dolorem quis. Corrupti.',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus sunt neque saepe sed hic ullam labore reiciendis natus ratione eligendi.',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequuntur et eligendi. Inventore esse rem, itaque consequatur porro impedit modi.',
+  },
+];
+
+export default people;
